920fd232946abd0ce0388e638c449c9c65fa1cf1
Hernan Veiras <hernan.veiras@gmail.com>
2017-07-22 09:32:43 -0300

    WIP

--- test/definitions/getDefinition.js
+++ test/definitions/getDefinition.js
@@ -3,8 +3,4 @@
 const fs = require('fs');
 const path = require('path');
 
-function getDefinition (fileName) {
-  return JSON.parse(fs.readFileSync(path.join(__dirname, fileName), 'utf8'));
-}
-
-module.exports = getDefinition;
+module.exports = fileName => JSON.parse(fs.readFileSync(path.join(__dirname, fileName), 'utf8'));
--- test/lib.compile.js
+++ test/lib.compile.js
@@ -10,64 +10,13 @@ describe('Compile', () => {
     expect(() => compile(def)).to.throw(Error, 'Program must have at least one activity');
   });
 
-  it.only('should compile program with script activity', () => {
-    const def = getDefinition('simple-script-activity.json');
+  it('should compile single script activity', () => {
+    const def = getDefinition('single-script-activity.json');
     console.log(compile(def));
   });
 
-  it('should compile program activity', () => {
-    const definition = {
-      "name": "addOrUpdateCustomer",
-      "type": "program",
-      "parameters": {
-        "firstName": {
-          "required": true,
-          "type": "string"
-        },
-        "lastName": {
-          "required": true,
-          "type": "string"
-        },
-        "customerId": {
-          "required": false,
-          "default": "",
-          "type": "string"
-        }
-      },
-      "activities": [
-        {
-          "name": "isAdd",
-          "type": "branch",
-          "exp": "isNilOrEmpty($(params).customerId)",
-          "true": [
-            {
-              "name": "addCustomer",
-              "type": "asyncCall",
-              "identifier": "callEndpoint",
-              "args": {
-                "_endpoint": "addcustomer",
-                "firtName": "$(params).firstName",
-                "lastName": "$(params).lastName"
-              }
-            }
-          ],
-          "false": [
-            {
-              "name": "updateCustomer",
-              "type": "asyncCall",
-              "identifier": "callEndpoint",
-              "args": {
-                "_endpoint": "updatecustomer",
-                "firtName": "$(params).firstName",
-                "lastName": "$(params).lastName",
-                "customerId": "$(params).customerId"
-              }
-            }
-          ]
-        }
-      ]
-    };
-
-    console.log(compile(definition));
+  it.only('should compile sequence script activity', () => {
+    const def = getDefinition('sequence-script-activity.json');
+    console.log(compile(def));
   });
-})
+});
